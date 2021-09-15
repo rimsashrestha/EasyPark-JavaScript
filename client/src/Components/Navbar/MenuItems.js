49d00fbab0a1e72b42d782defc0b022c974b0b37
Norbu <nlama@ttu.edu>
2021-09-15 16:20:02 -0500

    new files

---
+++ client/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems =[
+    {
+        title: 'Home',
+        url: '',
+        cName: 'nav-lonks'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-lonks'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-lonks'
+    },
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-lonks'
+    }
+]
