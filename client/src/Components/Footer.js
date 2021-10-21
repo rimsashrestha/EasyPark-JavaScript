b4972237079d6a98b261396073b83e8d30ff4bee
Norbu <nlama@ttu.edu>
2021-10-21 03:35:45 -0500

    updated footbar

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './footer.css'
 
 import styled from 'styled-components'
 
@@ -49,6 +48,14 @@ const Title = styled.h2`
 
 `
 
+const Title2 = styled.h3`
+    color:white;
+    max-width: 200px;
+    text-align: center;
+    margin-bottom:20px;
+
+`
+
 const Link = styled.a`
     color:white;
     margin-bottom:20px;
@@ -73,10 +80,10 @@ const Footer = () => {
             <Row>
                 <Column>
                     <Title>Easypark</Title>
-                    <Link>address</Link>
-                    <Link>city</Link>
-                    <Link>zip</Link>
-                    <Link>phone</Link>
+                    <Title2>Tech Tack</Title2>
+                    <Title2>Texas Tech University</Title2>
+                    <Title2>Lubbock-Texas</Title2>
+                    <Title2>United States of America</Title2>
                 </Column>
                 <Column>
                     <Title>Easypark</Title>
@@ -100,44 +107,3 @@ const Footer = () => {
 
 export default Footer
 
-
-
-
-// const Footer = () => {
-//     return (
-//         <div className="headings">
-//             <div className="container">
-//                 <div className="row">
-//                     <div className="column">
-//                         <h3>Easy ParK</h3>
-//                         <ul className="list" >
-//                             <li>123-456-7896</li>
-//                             <li>abc street</li>
-//                             <li>Lubbock, Texas, 79415</li>
-//                         </ul>
-//                     </div>
-//                     <div className="column">
-//                         <h3><a href = "/">Home</a></h3>
-//                         <h3><a href = "/guest">Guest</a></h3>
-//                         <h3><a href = "/about">About Us</a></h3>
-//                         <h3><a href = "/contact">Contact Us</a></h3>
-//                         <h3><a href = "/account">My Account</a></h3>
-       
-//                     </div>
-//                     <div className="col">
-//                         <h3>Social Medias</h3>
-//                         <ul className="list">
-//                             <li>Facebook</li>
-//                             <li>Youtube</li>
-//                             <li>Twitter</li>
-//                         </ul>
-//                     </div>
-//                 </div>
-    
-//             </div>
-    
-//         </div>
-//       );
-// }
-
-// export default Footer
